15c6a0c4e5e09efe1d811fe9704caa195f98fdc0
Kris Patel <krishp759@gmail.com>
2023-06-05 16:24:15 +0530

    Extra Spcae remover and compy text added

--- project3/src/components/Form.js
+++ project3/src/components/Form.js
@@ -20,6 +20,17 @@ export default function Form(props) {
     const cleartext = () =>{
         setText("");
     }
+    const handleCopy = () =>{
+        var newText = text ;
+        console.log(newText);
+        // newText.select();
+        navigator.clipboard.writeText(newText.value);
+    }
+
+    const handleExtraSpace = () =>{
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+    }
 
     
     const [text, setText] = useState("");
@@ -29,9 +40,11 @@ export default function Form(props) {
                 <label htmlFor="exampleFormControlTextarea1" className="form-label">{props.title}</label>
                 <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" onChange={changeText} value={text}></textarea>
             </div>
+            <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy</button>
             <button className="btn btn-primary mx-2" onClick={toUpper}>Uppercase</button>
             <button className="btn btn-primary mx-2" onClick={toLower}>Lowercase</button>
             <button className="btn btn-primary mx-2" onClick={cleartext}>Clear</button>
+            <button className="btn btn-primary mx-2" onClick={handleExtraSpace}>Remove Extra Space</button>
             <div className="my-5">
                 <div>
                     <p>Words {text.split(" ").length} and Charactors {text.length}</p>
